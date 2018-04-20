27d1a62ee42d54fb9f6dcd810445fad373a63d3d
Nov11 <529079634@qq.com>
2018-04-20 12:29:05 +0800

    the csv storage engine can now be built with mysqld

--- storage/oreilly-csv/ha_csv.cpp
+++ storage/oreilly-csv/ha_csv.cpp
@@ -2,58 +2,384 @@
 // Created by c6s on 18-4-16.
 //
 
+#inclunegative_class.h>
 #include "ha_csv.h"
+#include "table.h"
+#include "field.h"
 
 ha_csv::ha_csv(handlerton *ht_arg, TABLE_SHARE *share_arg) : handler(ht_arg, share_arg) {}
 
 int ha_csv::close(void) {
-    return 0;
+  /*
+     Clean up the lock structures, close the file handle, and
+     deallocate the data file descriptor memory.
+   */
+  thr_lock_delete(&thr_lock);
+  if (file) {
+    if (file->fd >= 0)
+      my_close(file->fd, MYF(0));
+    my_free(file);
+    file = 0;
+  }
+  return 0;
 }
 
+/* Called once for each record during a sequential table scan. */
 int ha_csv::rnd_next(uchar *buf) {
-    return 0;
+  /*
+   Increment the global statistics counter displayed by SHOW STATUS
+   under Handler_read_rnd_next.
+ */
+  ha_statistic_increment(&SSV::ha_read_rnd_next_count);
+
+  /* fetch_line() does the actual work. */
+  int error = fetch_line(buf);
+
+  /*
+    On success, update our estimate for the total number of records in the
+    table.
+  */
+  if (!error)
+    stats.records++;
+
+  /* Return whatever code we got from fetch_line() to the caller. */
+  return error;
 }
 
-int ha_csv::rnd_pos(uchar *buf, uchar *pos) {
-    return 0;
+/*
+  Positions the scan cursor at the position specified by set_pos and
+  read the record at that position. Used in GROUP BY and ORDER BY
+  optimization when the "filesort" technique is applied.
+*/
+int ha_csv::rnd_pos(uchar *buf, uchar *set_pos) {
+  ha_statistic_increment(&SSV::ha_read_rnd_count);
+  pos = my_get_ptr(set_pos, ref_length);
+  return fetch_line(buf);
 }
 
+/*
+  Stores the "position" reference to the current record in the ref
+  variable. At this point, this method is called in situation that are
+  impossible for this storage engine, but this could change in the future.
+ */
 void ha_csv::position(const uchar *record) {
-
+  my_store_ptr(ref, ref_length, pos);
 }
 
-int ha_csv::info(uint uint1) {
-    return 0;
+/* Updates the statistical variables in the handler object */
+int ha_csv::info(uint flags) {
+  /*
+    The optimizer must never think that the table has fewer than
+    two records unless this is indeed the case. Reporting a smaller number
+    makes the optimizer assume it needs to read no more than one record from
+    the table. Our storage engine does always know the number of records,
+    and in many cases cannot even make a good guess. To be safe and to keep
+    things simple, we always report that we have at least 2 records.
+  */
+  if (stats.records < 2)
+    stats.records = 2;
+
+  /*
+    The rest of the variables merely appear in SHOW TABLE STATUS output and
+    do not affect the optimizer. For the purpose of this example they can
+    be set to 0.
+   */
+
+  stats.deleted = 0;
+  errkey = 0;
+  stats.mean_rec_length = 0;
+  stats.data_file_length = 0;
+  stats.index_file_length = 0;
+  stats.max_data_file_length = 0;
+  stats.delete_length = 0;
+  if (flags & HA_STATUS_AUTO)
+    stats.auto_increment_value = 1;
+  return 0;
 }
 
+/**
+ * for display purpose
+ * @return
+ */
 const char *ha_csv::table_type() const {
-    return nullptr;
+  return "oreilly's csv storage engine";
 }
 
+static const char *csv_ext[] = {".mycsv", 0};
 const char **ha_csv::bas_ext() const {
-    return nullptr;
+  return csv_ext;
 }
 
 ulong ha_csv::index_flags(uint idx, uint part, bool all_parts) const {
-    return 0;
+  return 0;
 }
 
 THR_LOCK_DATA **ha_csv::store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type) {
-    return nullptr;
+  return nullptr;
 }
 
 int ha_csv::open(const char *name, int mode, uint test_if_locked) {
-    return 0;
+  /* Initialize the lock structures used by the lock manager. */
+  thr_lock_init(&thr_lock);
+  thr_lock_data_init(&thr_lock, &lock, NULL);
+
+  /* Allocate memory for the data file descriptor. */
+  file = (CSV_INFO *) my_malloc(sizeof(CSV_INFO), MYF(MY_WME));
+  if (!file)
+    return 1;
+
+  /* Translate the name of the parameter 'name' into the data file name. */
+  fn_format(file->fname, name, "", ".csv",
+            MY_REPLACE_EXT | MY_UNPACK_FILENAME);
+
+  /*
+     Open the file, and save the file handle id in the data
+     file descriptor structure.
+   */
+  if ((file->fd = my_open(file->fname, mode, MYF(0))) < 0) {
+    int error = my_errno;
+    close();
+    return error;
+  }
+
+  /* Read operations start from the beginning of the file. */
+  pos = 0;
+  return 0;
 }
 
+/*
+   We need this function to report that the records member cannot be used
+   to optimize SELECT COUNT(*) without a WHERE clause. Note that the value
+   records actually shows the correct count after a full scan, and can
+   indeed be used to optimize SELECT COUNT(*). This is left as an exercise
+   for the reader.
+ */
 handler::Table_flags ha_csv::table_flags(void) const {
-    return 0;
+//  return HA_NOT_EXACT_COUNT; <- this macro is not valid any more. I cannot find an equivalent
+  return HA_NO_TRANSACTIONS;
 }
 
+/* Nothing special to do on the storage engine level when the table
+   is created. The .CSV file is placed externally into the data directory.
+*/
 int ha_csv::create(const char *name, TABLE *form, HA_CREATE_INFO *info) {
-    return 0;
+  return 0;
 }
 
-int ha_csv::fetch_line(uchar *buf) {
-    return 0;
+int ha_csv::fetch_line(uchar *buffer) {
+  /*
+     Keep track of the current offset in the file as we read
+     portions of the line into a buffer.
+     Start at the current read cursor position.
+   */
+  my_off_t cur_pos = pos;
+
+  /*
+     We will use this to iterate through the array of
+     table field pointers to store the parsed data in the right
+     place and the right format.
+   */
+  Field **field = table->field;
+
+  /*
+    Used in parsing to remember the previous character. The impossible
+    value of 256 indicates that the last character either did not exist
+    (we are on the first one), or its value is irrelevant.
+  */
+  int last_c = 256;
+
+  /* Set to 1 if we are inside a quoted string. */
+  int in_quote = 0;
+
+  /* How many bytes we have seen so far in this line. */
+  uint bytes_parsed = 0;
+
+  /* Loop breaker flag. */
+  int line_read_done = 0;
+
+  /* Truncate the field value buffer. */
+  field_buf.length(0);
+
+  /* Attempt to read a whole line. */
+  for (; !line_read_done;) {
+    /* Read a block into a local buffer and deal with errors. */
+    uchar buf[CSV_READ_BLOCK_SIZE];
+    size_t bytes_read = my_pread(file->fd, buf, sizeof(buf), cur_pos, MYF(MY_WME));
+    if (bytes_read == MY_FILE_ERROR)
+      return HA_ERR_END_OF_FILE;
+    if (!bytes_read)
+      return HA_ERR_END_OF_FILE;
+
+    /*
+      If we reach this point, the read was successful. Start parsing the
+      data we have read.
+    */
+    uchar *p = buf;
+    uchar *buf_end = buf + bytes_read;
+
+    /* For each byte in the buffer. */
+    for (; p < buf_end;) {
+      char c = *p;
+      int end_of_line = 0;
+      int end_of_field = 0;
+      int char_escaped = 0;
+
+      switch (c) {
+        /*
+          A double-quote marks the start or the end of a quoted string
+          unless it has been escaped.
+        */
+        case '"':
+          if (last_c == '"' || last_c == '\\') {
+            field_buf.append(c);
+            char_escaped = 1;
+
+            /*
+              When we see the first quote, in_quote will get flipped.
+              A subsequent quote, however, tells us we are still inside the
+              quoted string.
+            */
+            if (last_c == '"')
+              in_quote = 1;
+          } else
+            in_quote = !in_quote;
+          break;
+          /*
+            Treat the backward slash as an escape character.
+          */
+        case '\\':
+          if (last_c == '\\') {
+            field_buf.append(c);
+            char_escaped = 1;
+          }
+          break;
+
+          /*
+            Set the termination flags on end-of-line unless it is quoted.
+          */
+        case '\r':
+        case '\n':
+          if (in_quote) {
+            field_buf.append(c);
+          } else {
+            end_of_line = 1;
+            end_of_field = 1;
+          }
+          break;
+
+          /* Comma signifies end-of-field unless quoted. */
+        case ',':
+          if (in_quote) {
+            field_buf.append(c);
+          } else
+            end_of_field = 1;
+          break;
+
+          /*
+            Regular charcters just get appended to the field
+            value buffer.
+          */
+        default:field_buf.append(c);
+          break;
+      }
+
+      /*
+        If at the end a field, and a matching field exists in the table
+        (it may not if the CSV file has extra fields), transfer the field
+        value buffer contents into the corresponding Field object. This
+        actually takes care of initializing the correct parts of the buffer
+        argument passed to us by the caller. The internal convention of the
+        optimizer dictates that the buffer pointers of the Field objects
+        must already be set up to point at the correct areas of the buffer
+        argument prior to calls to the data-retrieval methods of the handler
+        class.
+      */
+      if (end_of_field && *field) {
+        (*field)->store(field_buf.ptr(), field_buf.length(),
+                        system_charset_info);
+        field++;
+        field_buf.length(0);
+      }
+
+      /*
+        Special case - a character that was escaped itself should not be
+        regared as an escape character.
+      */
+      if (char_escaped)
+        last_c = 256;
+      else
+        last_c = c;
+      p++;
+
+      /* Prepare for loop exit on end-of-line. */
+      if (end_of_line) {
+        if (c == '\r')
+          p++;
+        line_read_done = 1;
+        in_quote = 0;
+        break;
+      }
+    }
+
+    /* Block read/parse cycle is complete - update the counters. */
+    bytes_parsed += (p - buf);
+    cur_pos += bytes_read;
+  }
+
+  /*
+    Now we are done with the line read/parsing. We still have a number
+    of small tasks left to complete the job.
+  */
+
+  /* Initialize the NULL indicator flags in the record. */
+  memset(buffer, 0, table->s->null_bytes);
+
+  /*
+    The parsed line may not have had the values of all of the fields.
+    Set the remaining fields to their default values.
+   */
+  for (; *field; field++) {
+    (*field)->set_default();
+  }
+
+  /* Move the cursor to the next record. */
+  pos += bytes_parsed;
+
+  /* Report success. */
+  return 0;
 }
+
+
+/* Defines the global structure for the plugin. */
+/*struct st_mysql_plugin
+{
+  int type;             *//* the plugin type (a MYSQL_XXX_PLUGIN value)   *//*
+  void *info;           *//* pointer to type-specific plugin descriptor   *//*
+  const char *name;     *//* plugin name                                  *//*
+  const char *author;   *//* plugin author (for I_S.PLUGINS)              *//*
+  const char *descr;    *//* general descriptive text (for I_S.PLUGINS)   *//*
+  int license;          *//* the plugin license (PLUGIN_LICENSE_XXX)      *//*
+  int (*init)(void *);  *//* the function to invoke when plugin is loaded *//*
+  int (*deinit)(void *);*//* the function to invoke when plugin is unloaded *//*
+  unsigned int version; *//* plugin version (for I_S.PLUGINS)             *//*
+  struct st_mysql_show_var *status_vars;
+  struct st_mysql_sys_var **system_vars;
+  void * __reserved1;   *//* reserved for dependency checking             *//*
+  unsigned long flags;  *//* flags for plugin *//*
+};*/
+
+mysql_declare_plugin(my_csv)
+        {
+            MYSQL_STORAGE_ENGINE_PLUGIN,
+            nullptr,
+            "plugin name :my csv storage engine",
+            "author:based on oreilly's mysql interals/ source code is modified to adopt them into version 5.5 from 5.1",
+            "description: useless storage engine",
+            PLUGIN_LICENSE_GPL,
+            nullptr,
+            nullptr,
+            0x0001,
+            nullptr,
+            nullptr,
+            0
+        }mysql_declare_plugin_end;
--- storage/oreilly-csv/ha_csv.h
+++ storage/oreilly-csv/ha_csv.h
@@ -7,77 +7,72 @@
 #ifndef MYSQL_HA_CSV_H
 #define MYSQL_HA_CSV_H
 
-
-#include <handler.h>
+#include <mysql_version.h>
+#include "handler.h"
 #include <sql_string.h>
 
 const int CSV_READ_BLOCK_SIZE = 512;
 
-struct CSV_INFO
-{
-    char fname[FN_REFLEN+1];
-    int fd;
-} ;
-
-class ha_csv : public handler{
-
-    /* Low-level storage engine data. */
-    CSV_INFO* file;
-
-    /* Lock structures for the table lock manager. */
-    THR_LOCK_DATA lock;
-    THR_LOCK thr_lock;
-
-    /* Table scan cursor.*/
-    my_off_t pos;
-
-    /* Buffer for reading CSV line blocks. */
-    char read_buf[CSV_READ_BLOCK_SIZE];
-
-    /* Buffer for parsing the field values. */
-    String field_buf;
-
-    /* See the comment in the implementation file. */
-    int fetch_line(uchar * buf);
-
-    /* this might be outdated from 5.1 */
-    int records = 0;
-public:
-    ha_csv(handlerton *ht_arg, TABLE_SHARE *share_arg);
-
-    int close(void) override;
-
-    int rnd_next(uchar *buf) override;
-
-    int rnd_pos(uchar *buf, uchar *pos) override;
-
-    void position(const uchar *record) override;
-
-    int info(uint uint1) override;
-
-    const char *table_type() const override;
-
-    const char **bas_ext() const override;
-
-    ulong index_flags(uint idx, uint part, bool all_parts) const override;
-
-    THR_LOCK_DATA **store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type) override;
-
-private:
-    int open(const char *name, int mode, uint test_if_locked) override;
-
-    /* Initializes the storage engine object for a sequential scan. */
-    int rnd_init(bool scan) override{
-        pos = 0;
-        records = 0;
-        return 0;
-    }
-
-    Table_flags table_flags(void) const override;
-
-    int create(const char *name, TABLE *form, HA_CREATE_INFO *info) override;
-
+struct CSV_INFO {
+  char fname[FN_REFLEN + 1]{0};
+  int fd = -1;
 };
 
+class ha_csv : public handler {
+
+  /* Low-level storage engine data. */
+  CSV_INFO *file = nullptr;
+
+  /* Lock structures for the table lock manager. */
+  THR_LOCK_DATA lock;
+  THR_LOCK thr_lock;
+
+  /* Table scan cursor.*/
+  my_off_t pos;
+
+  /* Buffer for reading CSV line blocks. */
+  char read_buf[CSV_READ_BLOCK_SIZE];
+
+  /* Buffer for parsing the field values. */
+  String field_buf;
+
+  /* read line into the caller-provided record buffer */
+  int fetch_line(uchar *buffer);
+ public:
+  ha_csv(handlerton *ht_arg, TABLE_SHARE *share_arg);
+
+  int close(void) override;
+
+  int rnd_next(uchar *buf) override;
+
+  int rnd_pos(uchar *buf, uchar *pos) override;
+
+  void position(const uchar *record) override;
+
+  int info(uint flags) override;
+
+  const char *table_type() const override;
+
+  const char **bas_ext() const override;
+
+  ulong index_flags(uint idx, uint part, bool all_parts) const override;
+
+  THR_LOCK_DATA **store_lock(THD *thd, THR_LOCK_DATA **to, enum thr_lock_type lock_type) override;
+
+ private:
+  int open(const char *name, int mode, uint test_if_locked) override;
+
+  /* Initializes the storage engine object for a sequential scan. */
+  int rnd_init(bool scan) override {
+    pos = 0;
+    stats.records = 0;
+    return 0;
+  }
+
+  Table_flags table_flags(void) const override;
+
+  int create(const char *name, TABLE *form, HA_CREATE_INFO *info) override;
+
+};
 
 #endif //MYSQL_HA_CSV_H
